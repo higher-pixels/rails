b8703aedfd5cb28037f9e5399239e2b224008c4b
Ron Shinall <81988008+ron-shinall@users.noreply.github.com>
2023-10-12 08:38:34 -0600

    Adding a negative test

--- activesupport/test/core_ext/module/association_aliasing_test.rb
+++ activesupport/test/core_ext/module/association_aliasing_test.rb
@@ -14,17 +14,24 @@ class AssociationAliasingTest < ActiveSupport::TestCase
     alias_association :person, :patient
   end
 
+  class Note < ActiveRecord::Base
+    validates_presence_of :message
+    alias_association :description, :message
+  end
+
   setup do
     ActiveRecord::Base.establish_connection({ adapter: "sqlite3", database: ":memory:" })
     @connection = ActiveRecord::Base.connection
     @connection.create_table(:patients)
     @connection.create_table(:tickets) { |t| t.integer :patient_id }
+    @connection.create_table(:notes) { |t| t.string :message }
     @patient = Patient.create!
   end
 
   teardown do
     @connection.drop_table :patients
     @connection.drop_table :tickets
+    @connection.drop_table :notes
   end
 
   def test_association_alias_getter
@@ -34,7 +41,11 @@ class AssociationAliasingTest < ActiveSupport::TestCase
 
   def test_association_alias_setter
     ticket = Ticket.create!(person: @patient)
-
     assert_equal @patient, ticket.patient
+  end
+
+  def not_work_for_attribute
+    assert Note.create!(message: "This should work.")
+    assert_not Note.create!(description: "This should not work.")
   end
 end
